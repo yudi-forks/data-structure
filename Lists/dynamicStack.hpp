ca04d8a7fe2fd28caf9fbdb7394b13c40457612f
GuiCT49 <gtomiasi@gmail.com>
2021-06-10 03:24:07 -0300

    adding DynamicStack destructor, and comments

--- Lists/dynamicStack.hpp
+++ Lists/dynamicStack.hpp
@@ -13,24 +13,34 @@ private:
     // Probably not the best way to do class heritage :p
 
 public:
+    // Calling LinkedList default constructor: null stack.
     DynamicStack () {
         LinkedList<T> noArgument;
         listStack = noArgument;
     }
 
+    // Calling LinkedList constructor w/argument: stack with one value.
     DynamicStack (T firstValue) {
         LinkedList<T> oneArgument (firstValue);
         listStack = oneArgument;
     }
 
+    // Calling LinkedList destructor: turning the stack null.
+    ~DynamicStack () {
+        listStack.~LinkedList();
+    }
+
+    // Calling push function, adding element to start of the list (stack).
     void push (T insertedElement) {
         listStack.addToStart (insertedElement);
     }
 
+    // Calling pop function, removing element from start of the list (stack).
     void pop () {
         listStack.removeFromStart ();
     }
 
+    // Calling top function, returning element from start of the list (stack).
     T top () {
         if (listStack.getLength() == 0) {
             printf("Error: can't return top value when stack is empty.\n");
